06da62c1fec9d3c1b33601e26c37469047ac77fa
ChrisGarozzo <garozzochristian@gmail.com>
2024-01-08 15:31:42 +0100

    added header component

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<script>
+export default{
+    name: "Header",
+}
+
+</script>
+
+<template>
+    <header>
+        <img src="../assets/ygoprodeck_header_logo.png" alt="" srcset="">
+        <h1>YU-Gi-Oh API</h1>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+@use '../style/partials/variables' as *;
+@use '../style/partials/mixins' as *;
+
+header{
+    @include flexCenter;
+    padding: 30px;
+    img{
+        padding-right: 20px;
+    }
+    background-color: aliceblue;
+}
+
+
+</style>
